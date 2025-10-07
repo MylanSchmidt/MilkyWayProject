0c06369f42ab38b3eb51a76edfb9ea9682d4c74a
MylanSchmidt <Mylan_Schmidt@mail.tmcc.edu>
2025-10-06 21:59:19 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,50 @@
+// Twinkling Starfield Animation using Canvas API 
+const canvas = document.getElementById("starCanvas");
+const ctx = canvas.getContext("2d");
+
+function resizeCanvas() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+resizeCanvas();
+
+window.addEventListener("resize", resizeCanvas);
+
+let stars = [];
+const numStars = 200;
+
+for (let i = 0; i < numStars; i++) {
+  stars.push({
+    x: Math.random() * canvas.width,
+    y: Math.random() * canvas.height,
+    r: Math.random() * 1.5,
+    d: Math.random() * 0.5,
+  });
+}
+
+function drawStars() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "white";
+  for (let s of stars) {
+    ctx.beginPath();
+    ctx.arc(s.x, s.y, s.r, 0, Math.PI * 2);
+    ctx.fill();
+  }
+}
+
+function moveStars() {
+  for (let s of stars) {
+    s.y += s.d;
+    if (s.y > canvas.height) {
+      s.y = 0;
+      s.x = Math.random() * canvas.width;
+    }
+  }
+}
+
+function animate() {
+  drawStars();
+  moveStars();
+  requestAnimationFrame(animate);
+}
+animate();
